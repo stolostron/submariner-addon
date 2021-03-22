67f4696696ea88688d470ca30dd210f7235f6e91
Zhiwei Yin <zyin@redhat.com>
2021-03-22 22:15:55 +0800

    upgrade crd to v1

--- go.mod
+++ go.mod
@@ -3,28 +3,28 @@ module github.com/open-cluster-management/submariner-addon
 go 1.15
 
 require (
-	github.com/aws/aws-sdk-go v1.35.32
+	github.com/aws/aws-sdk-go v1.38.1
 	github.com/ghodss/yaml v1.0.0
 	github.com/go-bindata/go-bindata v3.1.2+incompatible
-	github.com/golang/mock v1.3.1
-	github.com/onsi/ginkgo v1.14.0
-	github.com/onsi/gomega v1.10.1
-	github.com/open-cluster-management/api v0.0.0-20201126023000-353dd8370f4d
-	github.com/openshift/api v0.0.0-20200827090112-c05698d102cf
-	github.com/openshift/build-machinery-go v0.0.0-20200819073603-48aa266c95f7
-	github.com/openshift/library-go v0.0.0-20200902171820-35f48b6ef30c
-	github.com/spf13/cobra v1.0.0
+	github.com/golang/mock v1.4.1
+	github.com/onsi/ginkgo v1.14.1
+	github.com/onsi/gomega v1.10.2
+	github.com/open-cluster-management/api v0.0.0-20201210143210-581cab55c797
+	github.com/openshift/api v0.0.0-20201214114959-164a2fb63b5f
+	github.com/openshift/build-machinery-go v0.0.0-20210209125900-0da259a2c359
+	github.com/openshift/library-go v0.0.0-20210318152630-323ad8a8f7d8
+	github.com/spf13/cobra v1.1.1
 	github.com/spf13/pflag v1.0.5
-	golang.org/x/oauth2 v0.0.0-20191202225959-858c2ad4c8b6
-	google.golang.org/api v0.15.0
-	k8s.io/api v0.19.0
-	k8s.io/apiextensions-apiserver v0.19.0
-	k8s.io/apimachinery v0.19.0
-	k8s.io/client-go v0.19.0
-	k8s.io/code-generator v0.19.0
-	k8s.io/component-base v0.19.0
-	k8s.io/klog/v2 v2.3.0
-	sigs.k8s.io/controller-runtime v0.6.1-0.20200829232221-efc74d056b24
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+	google.golang.org/api v0.20.0
+	k8s.io/api v0.20.2
+	k8s.io/apiextensions-apiserver v0.20.1
+	k8s.io/apimachinery v0.20.2
+	k8s.io/client-go v0.20.2
+	k8s.io/code-generator v0.20.1
+	k8s.io/component-base v0.20.2
+	k8s.io/klog/v2 v2.4.0
+	sigs.k8s.io/controller-runtime v0.8.3
 )
 
 // ensure compatible between controller-runtime and kube-openapi
--- vendor/github.com/openshift/library-go/pkg/controller/factory/base_controller.go
+++ vendor/github.com/openshift/library-go/pkg/controller/factory/base_controller.go
@@ -62,6 +62,18 @@ func (s *scheduledJob) Run() {
 	s.queue.Add(DefaultQueueKey)
 }
 
+func waitForNamedCacheSync(controllerName string, stopCh <-chan struct{}, cacheSyncs ...cache.InformerSynced) error {
+	klog.Infof("Waiting for caches to sync for %s", controllerName)
+
+	if !cache.WaitForCacheSync(stopCh, cacheSyncs...) {
+		return fmt.Errorf("unable to sync caches for %s", controllerName)
+	}
+
+	klog.Infof("Caches are synced for %s ", controllerName)
+
+	return nil
+}
+
 func (c *baseController) Run(ctx context.Context, workers int) {
 	// HandleCrash recovers panics
 	defer utilruntime.HandleCrash()
@@ -69,12 +81,19 @@ func (c *baseController) Run(ctx context.Context, workers int) {
 	// give caches 10 minutes to sync
 	cacheSyncCtx, cacheSyncCancel := context.WithTimeout(ctx, c.cacheSyncTimeout)
 	defer cacheSyncCancel()
-	if !cache.WaitForNamedCacheSync(c.name, cacheSyncCtx.Done(), c.cachesToSync...) {
-		// the parent context is closed, it means we are shutting down, do not call panic()
-		if ctx.Err() != nil {
+	err := waitForNamedCacheSync(c.name, cacheSyncCtx.Done(), c.cachesToSync...)
+	if err != nil {
+		select {
+		case <-ctx.Done():
+			// Exit gracefully because the controller was requested to stop.
 			return
+		default:
+			// If caches did not sync after 10 minutes, it has taken oddly long and
+			// we should provide feedback. Since the control loops will never start,
+			// it is safer to exit with a good message than to continue with a dead loop.
+			// TODO: Consider making this behavior configurable.
+			klog.Exit(err)
 		}
-		panic("failed to wait for cache to sync")
 	}
 
 	var workerWg sync.WaitGroup
@@ -228,7 +247,11 @@ func (c *baseController) processNextWorkItem(queueCtx context.Context) {
 			// logging this helps detecting wedged controllers with missing pre-requirements
 			klog.V(5).Infof("%q controller requested synthetic requeue with key %q", c.name, key)
 		} else {
-			utilruntime.HandleError(fmt.Errorf("%q controller failed to sync %q, err: %w", c.name, key, err))
+			if klog.V(4).Enabled() || key != "key" {
+				utilruntime.HandleError(fmt.Errorf("%q controller failed to sync %q, err: %w", c.name, key, err))
+			} else {
+				utilruntime.HandleError(fmt.Errorf("%s reconciliation failed: %w", c.name, err))
+			}
 		}
 		c.syncContext.Queue().AddRateLimited(key)
 		return
