214239344b6bae70024bc4a5c833d62e85a67c7c
Tom Pantelis <tompantelis@gmail.com>
2021-08-12 16:10:24 -0400

    Convert the sub agent addon tests to use the Gingko framework
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- pkg/hub/submarineraddonagent/agent_test.go
+++ pkg/hub/submarineraddonagent/agent_test.go
@@ -1,154 +1,263 @@
-package submarineraddonagent
+package submarineraddonagent_test
 
 import (
+	"context"
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"encoding/pem"
+	"fmt"
 	"math/rand"
 	"net"
 	"testing"
 
-	testinghelpers "github.com/open-cluster-management/submariner-addon/pkg/helpers/testing"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/open-cluster-management/submariner-addon/pkg/hub/submarineraddonagent"
+	"github.com/openshift/library-go/pkg/operator/events"
+	appsv1 "k8s.io/api/apps/v1"
+	certificatesv1 "k8s.io/api/certificates/v1"
+	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes"
+	kubefake "k8s.io/client-go/kubernetes/fake"
+	"open-cluster-management.io/addon-framework/pkg/agent"
 	addonapiv1alpha1 "open-cluster-management.io/api/addon/v1alpha1"
 	clusterv1 "open-cluster-management.io/api/cluster/v1"
-
-	"github.com/openshift/library-go/pkg/operator/events/eventstesting"
-
-	certificatesv1 "k8s.io/api/certificates/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	kubefake "k8s.io/client-go/kubernetes/fake"
 )
 
-func TestManifests(t *testing.T) {
-	cases := []struct {
-		name              string
-		addOn             *addonapiv1alpha1.ManagedClusterAddOn
-		expectedFileCount int
-	}{
-		{
-			name:              "using default installation namespace",
-			addOn:             &addonapiv1alpha1.ManagedClusterAddOn{},
-			expectedFileCount: 6,
-		},
-		{
-			name: "using customized installation namespace",
-			addOn: &addonapiv1alpha1.ManagedClusterAddOn{
-				Spec: addonapiv1alpha1.ManagedClusterAddOnSpec{
-					InstallNamespace: "test",
-				},
-			},
-			expectedFileCount: 7,
-		},
-	}
-
-	for _, c := range cases {
-		t.Run(c.name, func(t *testing.T) {
-			agent := &addOnAgent{agentImage: "test"}
-			actualFiles, err := agent.Manifests(&clusterv1.ManagedCluster{}, c.addOn)
-			if err != nil {
-				t.Errorf("unexpected error %v", err)
-			}
-			if len(actualFiles) != c.expectedFileCount {
-				t.Errorf("expected %d files, but get %d, %v", c.expectedFileCount, len(actualFiles), actualFiles)
-			}
-		})
-	}
+func TestSubmarinerAddOnAgent(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Submariner AddOn Agent Suite")
 }
 
-func TestPermissionConfig(t *testing.T) {
-	kubeClient := kubefake.NewSimpleClientset()
-	agent := NewAddOnAgent(kubeClient, eventstesting.NewTestingEventRecorder(t), "test")
-	err := agent.permissionConfig(&clusterv1.ManagedCluster{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "test",
-		},
-	}, &addonapiv1alpha1.ManagedClusterAddOn{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "test",
-		},
+var _ = Describe("Manifests", func() {
+	t := newTestDriver()
+
+	Context("using the default installation namespace", func() {
+		It("should return the correct resources", func() {
+			objs, err := t.addOnAgent.Manifests(&clusterv1.ManagedCluster{}, &addonapiv1alpha1.ManagedClusterAddOn{})
+			Expect(err).To(Succeed())
+
+			verifyManifestObjs(objs, defaultManifestObjStrings("open-cluster-management-agent-addon"))
+		})
 	})
-	if err != nil {
-		t.Errorf("unexpected error %v", err)
-	}
-	testinghelpers.AssertActions(t, kubeClient.Actions(), "get", "create", "get", "create")
-}
 
-func TestCSRApproveCheck(t *testing.T) {
-	cases := []struct {
-		name    string
-		csr     csrHolder
-		checked bool
-	}{
-		{
-			name: "an invalid signer name",
-			csr: csrHolder{
-				SignerName: "invalidsigner",
-			},
-			checked: false,
-		},
-		{
-			name: "a wrong block type",
-			csr: csrHolder{
-				SignerName:   certificatesv1.KubeAPIServerClientSignerName,
-				ReqBlockType: "RSA PRIVATE KEY",
-			},
-			checked: false,
-		},
-		{
-			name: "an empty organization",
-			csr: csrHolder{
-				SignerName:   certificatesv1.KubeAPIServerClientSignerName,
-				ReqBlockType: "CERTIFICATE REQUEST",
-			},
-			checked: false,
-		},
-		{
-			name: "an invalid common name",
-			csr: csrHolder{
+	Context("using a custom installation namespace", func() {
+		It("should return the correct resources", func() {
+			ns := "custom"
+			objs, err := t.addOnAgent.Manifests(&clusterv1.ManagedCluster{}, &addonapiv1alpha1.ManagedClusterAddOn{
+				Spec: addonapiv1alpha1.ManagedClusterAddOnSpec{
+					InstallNamespace: ns,
+				},
+			})
+			Expect(err).To(Succeed())
+
+			verifyManifestObjs(objs, append(defaultManifestObjStrings(ns),
+				toManifestObjString(&corev1.Namespace{ObjectMeta: metav1.ObjectMeta{
+					Name: ns,
+				}})))
+		})
+	})
+})
+
+var _ = Describe("GetAgentAddonOptions", func() {
+	clusterName := "test-managed-cluster"
+	t := newTestDriver()
+
+	var options agent.AgentAddonOptions
+
+	JustBeforeEach(func() {
+		options = t.addOnAgent.GetAgentAddonOptions()
+	})
+
+	Context("PermissionConfig", func() {
+		It("should create the hub permission resources", func() {
+			options.Registration.PermissionConfig(&clusterv1.ManagedCluster{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: clusterName,
+				},
+			}, &addonapiv1alpha1.ManagedClusterAddOn{})
+
+			_, err := t.kubeClient.RbacV1().Roles(clusterName).Get(context.TODO(),
+				"open-cluster-management:submariner-addon:agent", metav1.GetOptions{})
+			Expect(err).To(Succeed())
+
+			_, err = t.kubeClient.RbacV1().RoleBindings(clusterName).Get(context.TODO(),
+				"open-cluster-management:submariner-addon:agent", metav1.GetOptions{})
+			Expect(err).To(Succeed())
+		})
+	})
+
+	Context("CSRApproveCheck", func() {
+		var csr csrHolder
+		var result bool
+
+		BeforeEach(func() {
+			csr = csrHolder{
 				SignerName:   certificatesv1.KubeAPIServerClientSignerName,
 				ReqBlockType: "CERTIFICATE REQUEST",
 				Orgs: []string{
 					"system:authenticated",
 					"system:open-cluster-management:addon:submariner",
-					"system:open-cluster-management:cluster:test:addon:submariner",
+					"system:open-cluster-management:cluster:" + clusterName + ":addon:submariner",
 				},
-				CN: "system:open-cluster-management:cluster:test:addon:submariner:agent:invalid",
-			},
-			checked: false,
-		},
-		{
-			name: "a valid csr",
-			csr: csrHolder{
-				SignerName:   certificatesv1.KubeAPIServerClientSignerName,
-				ReqBlockType: "CERTIFICATE REQUEST",
-				Orgs: []string{
-					"system:authenticated",
-					"system:open-cluster-management:addon:submariner",
-					"system:open-cluster-management:cluster:test:addon:submariner",
-				},
-				CN: "system:open-cluster-management:cluster:test:addon:submariner:agent:submariner-addon-agent",
-			},
-			checked: true,
-		},
-	}
-
-	for _, c := range cases {
-		t.Run(c.name, func(t *testing.T) {
-			agent := &addOnAgent{}
-			if checked := agent.csrApproveCheck(&clusterv1.ManagedCluster{
-				ObjectMeta: metav1.ObjectMeta{
-					Name: "test",
-				},
-			}, &addonapiv1alpha1.ManagedClusterAddOn{
-				ObjectMeta: metav1.ObjectMeta{
-					Name: "submariner-addon",
-				},
-			}, newCSR(c.csr)); checked != c.checked {
-				t.Errorf("expected %t, but failed", c.checked)
+				CN: "system:open-cluster-management:cluster:" + clusterName + ":addon:submariner:agent:submariner-addon-agent",
 			}
 		})
+
+		JustBeforeEach(func() {
+			result = options.Registration.CSRApproveCheck(&clusterv1.ManagedCluster{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: clusterName,
+				},
+			}, &addonapiv1alpha1.ManagedClusterAddOn{}, newCSR(csr))
+		})
+
+		When("the CSR is valid", func() {
+			It("should return true", func() {
+				Expect(result).To(BeTrue())
+			})
+		})
+
+		When("the signer name is invalid", func() {
+			BeforeEach(func() {
+				csr.SignerName = "invalid"
+			})
+
+			It("should return false", func() {
+				Expect(result).To(BeFalse())
+			})
+		})
+
+		When("the block type is invalid", func() {
+			BeforeEach(func() {
+				csr.ReqBlockType = "RSA PRIVATE KEY"
+			})
+
+			It("should return false", func() {
+				Expect(result).To(BeFalse())
+			})
+		})
+
+		When("the authenticated group is missing from the orgs", func() {
+			BeforeEach(func() {
+				csr.Orgs[0] = "invalid"
+			})
+
+			It("should return false", func() {
+				Expect(result).To(BeFalse())
+			})
+		})
+
+		When("the addon group is missing from the orgs", func() {
+			BeforeEach(func() {
+				csr.Orgs[1] = "invalid"
+			})
+
+			It("should return false", func() {
+				Expect(result).To(BeFalse())
+			})
+		})
+
+		When("the cluster addon group is missing from the orgs", func() {
+			BeforeEach(func() {
+				csr.Orgs[2] = "invalid"
+			})
+
+			It("should return false", func() {
+				Expect(result).To(BeFalse())
+			})
+		})
+
+		When("an extra org is present", func() {
+			BeforeEach(func() {
+				csr.Orgs = append(csr.Orgs, "extra")
+			})
+
+			It("should return false", func() {
+				Expect(result).To(BeFalse())
+			})
+		})
+
+		When("the common name is invalid", func() {
+			BeforeEach(func() {
+				csr.CN = "invalid"
+			})
+
+			It("should return false", func() {
+				Expect(result).To(BeFalse())
+			})
+		})
+	})
+})
+
+type testDriver struct {
+	addOnAgent agent.AgentAddon
+	kubeClient kubernetes.Interface
+}
+
+func newTestDriver() *testDriver {
+	t := &testDriver{}
+
+	BeforeEach(func() {
+		t.kubeClient = kubefake.NewSimpleClientset()
+		t.addOnAgent = submarineraddonagent.NewAddOnAgent(t.kubeClient, events.NewLoggingEventRecorder("test"), "test")
+	})
+
+	return t
+}
+
+func verifyManifestObjs(actualObjs []runtime.Object, expected []string) {
+	actual := map[string]bool{}
+	for i := range actualObjs {
+		actual[toManifestObjString(actualObjs[i])] = true
+	}
+
+	for i := range expected {
+		_, found := actual[expected[i]]
+		Expect(found).To(BeTrue(), "Missing expected manifest resource %q", expected[i])
+		delete(actual, expected[i])
+	}
+
+	Expect(actual).To(BeEmpty(), "Received unexpected manifest resources")
+}
+
+func toManifestObjString(obj runtime.Object) string {
+	m, err := meta.Accessor(obj)
+	Expect(err).To(Succeed())
+
+	return fmt.Sprintf("Type: %T, NS: %s, Name: %s", obj, m.GetNamespace(), m.GetName())
+}
+
+func defaultManifestObjStrings(ns string) []string {
+	return []string{
+		toManifestObjString(&rbacv1.ClusterRole{ObjectMeta: metav1.ObjectMeta{
+			Name: "open-cluster-management:submariner-addon:agent",
+		}}),
+		toManifestObjString(&rbacv1.ClusterRoleBinding{ObjectMeta: metav1.ObjectMeta{
+			Name: "open-cluster-management:submariner-addon:agent",
+		}}),
+		toManifestObjString(&appsv1.Deployment{ObjectMeta: metav1.ObjectMeta{
+			Name:      "submariner-addon",
+			Namespace: ns,
+		}}),
+		toManifestObjString(&rbacv1.Role{ObjectMeta: metav1.ObjectMeta{
+			Name:      "submariner-addon",
+			Namespace: ns,
+		}}),
+		toManifestObjString(&rbacv1.RoleBinding{ObjectMeta: metav1.ObjectMeta{
+			Name:      "submariner-addon",
+			Namespace: ns,
+		}}),
+		toManifestObjString(&corev1.ServiceAccount{ObjectMeta: metav1.ObjectMeta{
+			Name:      "submariner-addon-sa",
+			Namespace: ns,
+		}}),
 	}
 }
 
@@ -162,9 +271,8 @@ type csrHolder struct {
 func newCSR(holder csrHolder) *certificatesv1.CertificateSigningRequest {
 	insecureRand := rand.New(rand.NewSource(0))
 	pk, err := ecdsa.GenerateKey(elliptic.P256(), insecureRand)
-	if err != nil {
-		panic(err)
-	}
+	Expect(err).To(Succeed())
+
 	csrb, err := x509.CreateCertificateRequest(insecureRand, &x509.CertificateRequest{
 		Subject: pkix.Name{
 			CommonName:   holder.CN,
@@ -174,9 +282,9 @@ func newCSR(holder csrHolder) *certificatesv1.CertificateSigningRequest {
 		EmailAddresses: []string{},
 		IPAddresses:    []net.IP{},
 	}, pk)
-	if err != nil {
-		panic(err)
-	}
+
+	Expect(err).To(Succeed())
+
 	return &certificatesv1.CertificateSigningRequest{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:         "test-csr",
