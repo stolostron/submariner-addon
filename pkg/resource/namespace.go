2b07131b9107909353c42afe28cf9864471cab60
Stephen Kitt <skitt@redhat.com>
2022-01-24 09:58:41 +0100

    Use os.ReadFile instead of deprecated ioutil
    ... as recommended by the Go developers, and required by our updated
    linting setup.
    
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- pkg/resource/namespace.go
+++ pkg/resource/namespace.go
@@ -1,11 +1,11 @@
 package resource
 
-import "io/ioutil"
+import "os"
 
 // GetCurrentNamespace returns the current namespace from file system,
 // if the namespace is not found, it returns the defaultNamespace.
 func GetCurrentNamespace(defaultNamespace string) string {
-	nsBytes, err := ioutil.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace")
+	nsBytes, err := os.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace")
 	if err != nil {
 		return defaultNamespace
 	}
