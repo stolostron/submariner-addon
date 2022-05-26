55fa0734d98d51ad08b0a20ecd9453345c55356e
Vishal Thapar <5137689+vthapar@users.noreply.github.com>
2022-05-26 12:24:41 +0530

    Check status of all deployments
    Adds code to check status of following deployments in addon agent:
     * LighthouseAgent
     * LighthouseCoreDNS
     * Globalnet
     * NetworkPluginSyncer
    
    Adds UT
    
    Signed-off-by: Vishal Thapar <5137689+vthapar@users.noreply.github.com>

--- pkg/spoke/submarineragent/deployment_controller_test.go
+++ pkg/spoke/submarineragent/deployment_controller_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/stolostron/submariner-addon/pkg/spoke/submarineragent"
 	fakereactor "github.com/submariner-io/admiral/pkg/fake"
 	"github.com/submariner-io/admiral/pkg/syncer/test"
+	submarinerv1alpha1 "github.com/submariner-io/submariner-operator/api/submariner/v1alpha1"
 	appsv1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/dynamic"
@@ -18,7 +19,10 @@ import (
 	"k8s.io/client-go/tools/cache"
 )
 
-const deploymentDegradedType string = "SubmarinerAgentDegraded"
+const (
+	deploymentDegradedType     = "SubmarinerAgentDegraded"
+	networkPluginOVNKubernetes = "OVNKubernetes"
+)
 
 var _ = Describe("Deployment Status Controller", func() {
 	t := newDeploymentControllerTestDriver()
@@ -78,7 +82,7 @@ var _ = Describe("Deployment Status Controller", func() {
 			t.awaitStatusCondition(metav1.ConditionTrue, "NoOperatorAvailable")
 
 			t.operatorDeployment.Status.AvailableReplicas = 1
-			t.updateOperatorDeployment()
+			t.updateDeployment(t.operatorDeployment)
 
 			t.awaitStatusConditionDeployed()
 		})
@@ -159,6 +163,144 @@ var _ = Describe("Deployment Status Controller", func() {
 		})
 	})
 
+	When("the lighthouse agent deployment doesn't initially exist", func() {
+		BeforeEach(func() {
+			t.lighthouseAgentDeployment = nil
+		})
+
+		It("should eventually update the ManagedClusterAddOn status condition from degraded to deployed", func() {
+			t.awaitStatusCondition(metav1.ConditionTrue, "NoLighthouseAgentDeployment")
+
+			t.lighthouseAgentDeployment = newLighthouseAgentDeployment()
+			t.createLighthouseAgentDeployment()
+
+			t.awaitStatusConditionDeployed()
+		})
+	})
+
+	When("no lighthouse agent deployment replica is initially available", func() {
+		BeforeEach(func() {
+			t.lighthouseAgentDeployment.Status.AvailableReplicas = 0
+		})
+
+		It("should eventually update the ManagedClusterAddOn status condition from degraded to deployed", func() {
+			t.awaitStatusCondition(metav1.ConditionTrue, "NoLighthouseAgentAvailable")
+
+			t.lighthouseAgentDeployment.Status.AvailableReplicas = 1
+			t.updateDeployment(t.lighthouseAgentDeployment)
+
+			t.awaitStatusConditionDeployed()
+		})
+	})
+
+	When("the lighthouse coredns deployment doesn't initially exist", func() {
+		BeforeEach(func() {
+			t.lighthouseCoreDNSDeployment = nil
+		})
+
+		It("should eventually update the ManagedClusterAddOn status condition from degraded to deployed", func() {
+			t.awaitStatusCondition(metav1.ConditionTrue, "NoLighthouseCoreDNSDeployment")
+
+			t.lighthouseCoreDNSDeployment = newLighthouseCoreDNSDeployment()
+			t.createLighthouseCoreDNSDeployment()
+
+			t.awaitStatusConditionDeployed()
+		})
+	})
+
+	When("no lighthouse coredns deployment replica is initially available", func() {
+		BeforeEach(func() {
+			t.lighthouseCoreDNSDeployment.Status.AvailableReplicas = 0
+		})
+
+		It("should eventually update the ManagedClusterAddOn status condition from degraded to deployed", func() {
+			t.awaitStatusCondition(metav1.ConditionTrue, "NoLighthouseCoreDNSAvailable")
+
+			t.lighthouseCoreDNSDeployment.Status.AvailableReplicas = 1
+			t.updateDeployment(t.lighthouseCoreDNSDeployment)
+
+			t.awaitStatusConditionDeployed()
+		})
+	})
+
+	When("globalnet is enabled", func() {
+		BeforeEach(func() {
+			t.submariner.Spec.GlobalCIDR = "242.0.0.0/16"
+			t.updateSubmariner()
+		})
+
+		When("the globalnet deployment doesn't initially exist", func() {
+			BeforeEach(func() {
+				t.globalnetDeployment = nil
+			})
+
+			It("should eventually update the ManagedClusterAddOn status condition from degraded to deployed", func() {
+				t.awaitStatusCondition(metav1.ConditionTrue, "NoGlobalnetDeployment")
+
+				t.globalnetDeployment = newGlobalnetDeployment()
+				t.createGlobalnetDeployment()
+
+				t.awaitStatusConditionDeployed()
+			})
+		})
+
+		When("no globalnet deployment replica is initially available", func() {
+			BeforeEach(func() {
+				t.globalnetDeployment = newGlobalnetDeployment()
+				t.globalnetDeployment.Status.AvailableReplicas = 0
+				t.createGlobalnetDeployment()
+			})
+
+			It("should eventually update the ManagedClusterAddOn status condition from degraded to deployed", func() {
+				t.awaitStatusCondition(metav1.ConditionTrue, "NoGlobalnetAvailable")
+
+				t.globalnetDeployment.Status.AvailableReplicas = 1
+				t.updateDeployment(t.globalnetDeployment)
+
+				t.awaitStatusConditionDeployed()
+			})
+		})
+	})
+
+	When("network plugin is OVNKubernetes", func() {
+		BeforeEach(func() {
+			t.submariner.Status.NetworkPlugin = networkPluginOVNKubernetes
+			t.updateSubmariner()
+		})
+
+		When("the networkplugin syncer deployment doesn't initially exist", func() {
+			BeforeEach(func() {
+				t.networkPluginSyncerDeployment = nil
+			})
+
+			It("should eventually update the ManagedClusterAddOn status condition from degraded to deployed", func() {
+				t.awaitStatusCondition(metav1.ConditionTrue, "NoNetworkPluginSyncerDeployment")
+
+				t.networkPluginSyncerDeployment = newNetworkPluginsyncerDeployment()
+				t.createNetworkPluginSyncerDeployment()
+
+				t.awaitStatusConditionDeployed()
+			})
+		})
+
+		When("no networkplugin syncer deployment replica is initially available", func() {
+			BeforeEach(func() {
+				t.networkPluginSyncerDeployment = newNetworkPluginsyncerDeployment()
+				t.networkPluginSyncerDeployment.Status.AvailableReplicas = 0
+				t.createNetworkPluginSyncerDeployment()
+			})
+
+			It("should eventually update the ManagedClusterAddOn status condition from degraded to deployed", func() {
+				t.awaitStatusCondition(metav1.ConditionTrue, "NoNetworkPluginSyncerAvailable")
+
+				t.networkPluginSyncerDeployment.Status.AvailableReplicas = 1
+				t.updateDeployment(t.networkPluginSyncerDeployment)
+
+				t.awaitStatusConditionDeployed()
+			})
+		})
+	})
+
 	When("updating the ManagedClusterAddOn status initially fails", func() {
 		Context("", func() {
 			BeforeEach(func() {
@@ -184,13 +326,19 @@ var _ = Describe("Deployment Status Controller", func() {
 
 type deploymentControllerTestDriver struct {
 	managedClusterAddOnTestBase
-	kubeClient          *kubeFake.Clientset
-	subscriptionClient  dynamic.ResourceInterface
-	subscription        *operatorsv1alpha1.Subscription
-	operatorDeployment  *appsv1.Deployment
-	gatewayDaemonSet    *appsv1.DaemonSet
-	routeAgentDaemonSet *appsv1.DaemonSet
-	stop                context.CancelFunc
+	kubeClient                    *kubeFake.Clientset
+	subscriptionClient            dynamic.ResourceInterface
+	submarinerClient              dynamic.ResourceInterface
+	subscription                  *operatorsv1alpha1.Subscription
+	submariner                    *submarinerv1alpha1.Submariner
+	operatorDeployment            *appsv1.Deployment
+	gatewayDaemonSet              *appsv1.DaemonSet
+	routeAgentDaemonSet           *appsv1.DaemonSet
+	lighthouseAgentDeployment     *appsv1.Deployment
+	lighthouseCoreDNSDeployment   *appsv1.Deployment
+	globalnetDeployment           *appsv1.Deployment
+	networkPluginSyncerDeployment *appsv1.Deployment
+	stop                          context.CancelFunc
 }
 
 func newDeploymentControllerTestDriver() *deploymentControllerTestDriver {
@@ -201,17 +349,26 @@ func newDeploymentControllerTestDriver() *deploymentControllerTestDriver {
 		t.managedClusterAddOnTestBase.init()
 
 		t.subscription = newSubscription()
+		t.submariner = newSubmariner()
 		t.operatorDeployment = newOperatorDeployment()
 		t.gatewayDaemonSet = newGatewayDaemonSet()
 		t.routeAgentDaemonSet = newRouteAgentDaemonSet()
+		t.lighthouseAgentDeployment = newLighthouseAgentDeployment()
+		t.lighthouseCoreDNSDeployment = newLighthouseCoreDNSDeployment()
 	})
 
 	JustBeforeEach(func() {
 		subscriptionClient, dynamicInformerFactory, subscriptionInformer := newDynamicClientWithInformer(submarinerNS)
+
 		t.subscriptionClient = subscriptionClient
+		t.submarinerClient = subscriptionClient
 
 		if t.subscription != nil {
 			t.createSubscription()
+		}
+
+		if t.submariner != nil {
+			t.createSubmariner()
 		}
 
 		if t.operatorDeployment != nil {
@@ -226,13 +383,21 @@ func newDeploymentControllerTestDriver() *deploymentControllerTestDriver {
 			t.createDaemonSet(t.routeAgentDaemonSet)
 		}
 
+		if t.lighthouseAgentDeployment != nil {
+			t.createLighthouseAgentDeployment()
+		}
+
+		if t.lighthouseCoreDNSDeployment != nil {
+			t.createLighthouseCoreDNSDeployment()
+		}
+
 		kubeInformerFactory := kubeInformers.NewSharedInformerFactory(t.kubeClient, 0)
 
 		t.managedClusterAddOnTestBase.run()
 
 		controller := submarineragent.NewDeploymentStatusController(clusterName, submarinerNS, t.addOnClient,
 			kubeInformerFactory.Apps().V1().DaemonSets(), kubeInformerFactory.Apps().V1().Deployments(),
-			subscriptionInformer, events.NewLoggingEventRecorder("test"))
+			subscriptionInformer, subscriptionInformer, events.NewLoggingEventRecorder("test"))
 
 		var ctx context.Context
 
@@ -259,8 +424,18 @@ func (t *deploymentControllerTestDriver) createSubscription() {
 	Expect(err).To(Succeed())
 }
 
+func (t *deploymentControllerTestDriver) createSubmariner() {
+	_, err := t.submarinerClient.Create(context.TODO(), test.ToUnstructured(t.submariner), metav1.CreateOptions{})
+	Expect(err).To(Succeed())
+}
+
 func (t *deploymentControllerTestDriver) updateSubscription() {
 	_, err := t.subscriptionClient.Update(context.TODO(), test.ToUnstructured(t.subscription), metav1.UpdateOptions{})
+	Expect(err).To(Succeed())
+}
+
+func (t *deploymentControllerTestDriver) updateSubmariner() {
+	_, err := t.submarinerClient.Update(context.TODO(), test.ToUnstructured(t.submariner), metav1.UpdateOptions{})
 	Expect(err).To(Succeed())
 }
 
@@ -269,8 +444,28 @@ func (t *deploymentControllerTestDriver) createOperatorDeployment() {
 	Expect(err).To(Succeed())
 }
 
-func (t *deploymentControllerTestDriver) updateOperatorDeployment() {
-	_, err := t.kubeClient.AppsV1().Deployments(submarinerNS).Update(context.TODO(), t.operatorDeployment, metav1.UpdateOptions{})
+func (t *deploymentControllerTestDriver) createLighthouseAgentDeployment() {
+	_, err := t.kubeClient.AppsV1().Deployments(submarinerNS).Create(context.TODO(), t.lighthouseAgentDeployment, metav1.CreateOptions{})
+	Expect(err).To(Succeed())
+}
+
+func (t *deploymentControllerTestDriver) createLighthouseCoreDNSDeployment() {
+	_, err := t.kubeClient.AppsV1().Deployments(submarinerNS).Create(context.TODO(), t.lighthouseCoreDNSDeployment, metav1.CreateOptions{})
+	Expect(err).To(Succeed())
+}
+
+func (t *deploymentControllerTestDriver) createGlobalnetDeployment() {
+	_, err := t.kubeClient.AppsV1().Deployments(submarinerNS).Create(context.TODO(), t.globalnetDeployment, metav1.CreateOptions{})
+	Expect(err).To(Succeed())
+}
+
+func (t *deploymentControllerTestDriver) createNetworkPluginSyncerDeployment() {
+	_, err := t.kubeClient.AppsV1().Deployments(submarinerNS).Create(context.TODO(), t.networkPluginSyncerDeployment, metav1.CreateOptions{})
+	Expect(err).To(Succeed())
+}
+
+func (t *deploymentControllerTestDriver) updateDeployment(deployment *appsv1.Deployment) {
+	_, err := t.kubeClient.AppsV1().Deployments(submarinerNS).Update(context.TODO(), deployment, metav1.UpdateOptions{})
 	Expect(err).To(Succeed())
 }
 
@@ -309,6 +504,21 @@ func newSubscription() *operatorsv1alpha1.Subscription {
 	}
 }
 
+func newSubmariner() *submarinerv1alpha1.Submariner {
+	return &submarinerv1alpha1.Submariner{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test",
+			Namespace: submarinerNS,
+		},
+		Spec: submarinerv1alpha1.SubmarinerSpec{
+			GlobalCIDR: "",
+		},
+		Status: submarinerv1alpha1.SubmarinerStatus{
+			NetworkPlugin: "OpenShiftSDN",
+		},
+	}
+}
+
 func newOperatorDeployment() *appsv1.Deployment {
 	return &appsv1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
@@ -338,6 +548,54 @@ func newRouteAgentDaemonSet() *appsv1.DaemonSet {
 		ObjectMeta: metav1.ObjectMeta{
 			Namespace: submarinerNS,
 			Name:      "submariner-routeagent",
+		},
+	}
+}
+
+func newLighthouseAgentDeployment() *appsv1.Deployment {
+	return &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: submarinerNS,
+			Name:      "submariner-lighthouse-agent",
+		},
+		Status: appsv1.DeploymentStatus{
+			AvailableReplicas: 1,
+		},
+	}
+}
+
+func newLighthouseCoreDNSDeployment() *appsv1.Deployment {
+	return &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: submarinerNS,
+			Name:      "submariner-lighthouse-coredns",
+		},
+		Status: appsv1.DeploymentStatus{
+			AvailableReplicas: 1,
+		},
+	}
+}
+
+func newGlobalnetDeployment() *appsv1.Deployment {
+	return &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: submarinerNS,
+			Name:      "submariner-globalnet",
+		},
+		Status: appsv1.DeploymentStatus{
+			AvailableReplicas: 1,
+		},
+	}
+}
+
+func newNetworkPluginsyncerDeployment() *appsv1.Deployment {
+	return &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: submarinerNS,
+			Name:      "submariner-netwworkplugin-syncer",
+		},
+		Status: appsv1.DeploymentStatus{
+			AvailableReplicas: 1,
 		},
 	}
 }
