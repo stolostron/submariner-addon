e0bf0efc9483c7c9201105cce840b428c62c2243
Stephen Kitt <skitt@redhat.com>
2022-07-07 16:06:17 +0200

    Bump Submariner to 0.13.0-rc1
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- vendor/github.com/aws/aws-sdk-go-v2/service/sso/internal/endpoints/endpoints.go
+++ vendor/github.com/aws/aws-sdk-go-v2/service/sso/internal/endpoints/endpoints.go
@@ -152,6 +152,14 @@ var defaultPartitions = endpoints.Partitions{
 				},
 			},
 			endpoints.EndpointKey{
+				Region: "ap-northeast-3",
+			}: endpoints.Endpoint{
+				Hostname: "portal.sso.ap-northeast-3.amazonaws.com",
+				CredentialScope: endpoints.CredentialScope{
+					Region: "ap-northeast-3",
+				},
+			},
+			endpoints.EndpointKey{
 				Region: "ap-south-1",
 			}: endpoints.Endpoint{
 				Hostname: "portal.sso.ap-south-1.amazonaws.com",
@@ -197,6 +205,14 @@ var defaultPartitions = endpoints.Partitions{
 				Hostname: "portal.sso.eu-north-1.amazonaws.com",
 				CredentialScope: endpoints.CredentialScope{
 					Region: "eu-north-1",
+				},
+			},
+			endpoints.EndpointKey{
+				Region: "eu-south-1",
+			}: endpoints.Endpoint{
+				Hostname: "portal.sso.eu-south-1.amazonaws.com",
+				CredentialScope: endpoints.CredentialScope{
+					Region: "eu-south-1",
 				},
 			},
 			endpoints.EndpointKey{
