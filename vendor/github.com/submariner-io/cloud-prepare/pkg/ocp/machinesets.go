a8afa1c171c3777b50d3ae55904a9f7852cb1e38
Stephen Kitt <skitt@redhat.com>
2023-02-27 14:28:39 +0100

    Bump to Submariner 0.13.4
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- vendor/github.com/submariner-io/cloud-prepare/pkg/ocp/machinesets.go
+++ vendor/github.com/submariner-io/cloud-prepare/pkg/ocp/machinesets.go
@@ -26,15 +26,21 @@ import (
 	"github.com/pkg/errors"
 	"github.com/submariner-io/admiral/pkg/resource"
 	"github.com/submariner-io/admiral/pkg/util"
+	v1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic"
 )
 
 //go:generate mockgen -source=./machinesets.go -destination=./fake/machineset.go -package=fake
+
+const (
+	SubmarinerGatewayLabel = "submariner.io/gateway"
+)
 
 // MachineSetDeployer can deploy and delete machinesets from OCP.
 type MachineSetDeployer interface {
@@ -45,8 +51,14 @@ type MachineSetDeployer interface {
 	// If an empty workerNodeList is passed, the API will internally query the worker nodes.
 	GetWorkerNodeImage(workerNodeList []string, machineSet *unstructured.Unstructured, infraID string) (string, error)
 
+	// List will list all the machineSets that have the submariner.io/gateway set to "true".
+	List() ([]unstructured.Unstructured, error)
+
 	// Delete will remove the given machineset.
 	Delete(machineSet *unstructured.Unstructured) error
+
+	// DeleteByName will remove the machineset with given name.
+	DeleteByName(name, namespace string) error
 }
 
 type k8sMachineSetDeployer struct {
@@ -71,12 +83,30 @@ func (msd *k8sMachineSetDeployer) clientFor(obj runtime.Object) (dynamic.Resourc
 	return msd.dynamicClient.Resource(*gvr).Namespace(machineSet.GetNamespace()), nil
 }
 
+func (msd *k8sMachineSetDeployer) clientForMsd(nameSpace string) dynamic.ResourceInterface {
+	groupName := "machine.openshift.io"
+	version := schema.GroupVersion{Group: groupName, Version: "v1beta1"}
+	machinesetGVR := schema.GroupVersionResource{
+		Group:    groupName,
+		Version:  version.Version,
+		Resource: "machinesets",
+	}
+
+	return msd.dynamicClient.Resource(machinesetGVR).Namespace(nameSpace)
+}
+
 func (msd *k8sMachineSetDeployer) GetWorkerNodeImage(workerNodeList []string, machineSet *unstructured.Unstructured,
 	infraID string,
 ) (string, error) {
-	machineSetClient, err := msd.clientFor(machineSet)
-	if err != nil {
-		return "", err
+	machineSetClient := msd.clientForMsd("openshift-machine-api")
+
+	if machineSet != nil {
+		var err error
+
+		machineSetClient, err = msd.clientFor(machineSet)
+		if err != nil {
+			return "", err
+		}
 	}
 
 	if len(workerNodeList) == 0 {
@@ -100,33 +130,45 @@ func (msd *k8sMachineSetDeployer) GetWorkerNodeImage(workerNodeList []string, ma
 			return "", errors.Wrapf(err, "error retrieving machine set %q", nodeName)
 		}
 
-		labels, found, _ := unstructured.NestedStringMap(existing.Object, "spec", "template", "metadata", "labels")
-		if found {
+		if labels, found, _ := unstructured.NestedStringMap(existing.Object, "spec", "template", "metadata", "labels"); found {
 			role := labels["machine.openshift.io/cluster-api-machine-role"]
 			if strings.Compare(strings.ToLower(role), "worker") != 0 {
 				continue
 			}
 		}
 
-		disks, _, _ := unstructured.NestedSlice(existing.Object, "spec", "template", "spec", "providerSpec", "value", "disks")
-		if len(disks) == 0 {
-			image, _, _ := unstructured.NestedString(existing.Object, "spec", "template", "spec", "providerSpec", "value", "image")
-			if image != "" {
-				return image, nil
-			}
-		} else {
-			for _, o := range disks {
-				disk := o.(map[string]interface{})
-
-				image, _, _ := unstructured.NestedString(disk, "image")
-				if image != "" {
-					return image, nil
-				}
-			}
+		if image := getImageFromMachineSet(existing); image != "" {
+			return image, nil
 		}
 	}
 
 	return "", fmt.Errorf("could not retrieve the image of one of the worker nodes from the infra %q", infraID)
+}
+
+func getImageFromMachineSet(existing *unstructured.Unstructured) string {
+	disks, _, _ := unstructured.NestedSlice(existing.Object, "spec", "template", "spec", "providerSpec", "value", "disks")
+	for _, o := range disks {
+		disk := o.(map[string]interface{})
+
+		image, _, _ := unstructured.NestedString(disk, "image")
+		if image != "" {
+			return image
+		}
+	}
+
+	image, _, _ := unstructured.NestedString(existing.Object, "spec", "template", "spec", "providerSpec", "value", "image")
+	if image != "" {
+		return image
+	}
+
+	// For MachineSets deployed in Azure.
+	image, _, _ = unstructured.NestedString(existing.Object, "spec", "template", "spec", "providerSpec",
+		"value", "image", "resourceID")
+	if image != "" {
+		return image
+	}
+
+	return ""
 }
 
 func (msd *k8sMachineSetDeployer) Deploy(machineSet *unstructured.Unstructured) error {
@@ -152,4 +194,61 @@ func (msd *k8sMachineSetDeployer) Delete(machineSet *unstructured.Unstructured) 
 	}
 
 	return errors.Wrapf(err, "error deleting machine set %q", machineSet.GetName())
+}
+
+func (msd *k8sMachineSetDeployer) DeleteByName(name, namespace string) error {
+	machineSetClient := msd.clientForMsd(namespace)
+
+	err := machineSetClient.Delete(context.TODO(), name, metav1.DeleteOptions{})
+	if apierrors.IsNotFound(err) {
+		return nil
+	}
+
+	return errors.Wrapf(err, "error deleting machine set %q", name)
+}
+
+func (msd *k8sMachineSetDeployer) List() ([]unstructured.Unstructured, error) {
+	machineSetClient := msd.clientForMsd("")
+
+	machineSetList, err := machineSetClient.List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to list machinesets")
+	}
+
+	var resultList []unstructured.Unstructured
+	machinesetItems := machineSetList.Items
+
+	for i := range machinesetItems {
+		labels, _, err := unstructured.NestedStringMap(machinesetItems[i].Object, "spec", "template", "spec", "metadata", "labels")
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to get label from machineset ")
+		}
+
+		if labels[SubmarinerGatewayLabel] == "true" {
+			resultList = append(resultList, machinesetItems[i])
+		}
+	}
+
+	return resultList, nil
+}
+
+func RemoveDuplicates(machineSets []unstructured.Unstructured, gwNodes []v1.Node) []v1.Node {
+	var resultNode []v1.Node
+
+	for i := 0; i < len(gwNodes); i++ {
+		addToResult := true
+
+		for i := 0; i < len(machineSets); i++ {
+			if strings.Contains(gwNodes[i].GetName(), machineSets[i].GetName()) {
+				addToResult = false
+				break
+			}
+		}
+
+		if addToResult {
+			resultNode = append(resultNode, gwNodes[i])
+		}
+	}
+
+	return resultNode
 }
