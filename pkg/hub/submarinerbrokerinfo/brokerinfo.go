b9ffe18d78a56a613458d013dde3f6afb8595bdc
Stephen Kitt <skitt@redhat.com>
2023-10-23 12:20:03 +0200

    Log a warning if the channel doesn't match the default
    Users can configure a non-default channel, but support needs to know
    about this, so log it.
    
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- pkg/hub/submarinerbrokerinfo/brokerinfo.go
+++ pkg/hub/submarinerbrokerinfo/brokerinfo.go
@@ -12,6 +12,7 @@ import (
 	"github.com/pkg/errors"
 	configv1alpha1 "github.com/stolostron/submariner-addon/pkg/apis/submarinerconfig/v1alpha1"
 	"github.com/stolostron/submariner-addon/pkg/constants"
+	"github.com/submariner-io/admiral/pkg/log"
 	"github.com/submariner-io/admiral/pkg/reporter"
 	"github.com/submariner-io/submariner-operator/pkg/discovery/globalnet"
 	corev1 "k8s.io/api/core/v1"
@@ -25,6 +26,7 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/klog/v2"
 	controllerclient "sigs.k8s.io/controller-runtime/pkg/client"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 const (
@@ -43,6 +45,8 @@ const (
 )
 
 var (
+	logger = log.Logger{Logger: logf.Log.WithName("BrokerInfo")}
+
 	infrastructureGVR = schema.GroupVersionResource{
 		Group:    "config.openshift.io",
 		Version:  "v1",
@@ -54,6 +58,8 @@ var (
 		Version:  "v1",
 		Resource: "apiservers",
 	}
+
+	loggedCatalogChannel string
 )
 
 type SubmarinerBrokerInfo struct {
@@ -226,6 +232,11 @@ func applySubmarinerConfig(brokerInfo *SubmarinerBrokerInfo, submarinerConfig *c
 
 	if submarinerConfig.Spec.SubscriptionConfig.InstallPlanApproval != "" {
 		brokerInfo.InstallPlanApproval = submarinerConfig.Spec.SubscriptionConfig.InstallPlanApproval
+	}
+
+	if brokerInfo.CatalogChannel != defaultCatalogChannel && brokerInfo.CatalogChannel != loggedCatalogChannel {
+		logger.Infof("Tracking non-default catalog channel %q (default is %q)", brokerInfo.CatalogChannel, defaultCatalogChannel)
+		loggedCatalogChannel = brokerInfo.CatalogChannel
 	}
 
 	applySubmarinerImageConfig(brokerInfo, submarinerConfig)
