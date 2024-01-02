296dfc2ba370f05eb5de4fdf536e8a08618c6788
Stephen Kitt <skitt@redhat.com>
2024-01-02 14:10:58 +0100

    Use kube_codegen.sh instead of generate-groups.sh
    ... as requested by the latter (which no longer works anyway).
    
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- hack/update-deepcopy.sh
+++ hack/update-deepcopy.sh
@@ -7,14 +7,14 @@ CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-ge
 
 verify="${VERIFY:-}"
 
-GOFLAGS="" bash ${CODEGEN_PKG}/generate-groups.sh "deepcopy" \
+GOFLAGS="" bash ${CODEGEN_PKG}/kube_codegen.sh "deepcopy" \
   github.com/stolostron/submariner-addon/generated \
   github.com/stolostron/submariner-addon/pkg/apis \
   "submarinerconfig:v1alpha1" \
   --go-header-file ${SCRIPT_ROOT}/hack/empty.txt \
   ${verify}
 
-GOFLAGS="" bash ${CODEGEN_PKG}/generate-groups.sh "deepcopy" \
+GOFLAGS="" bash ${CODEGEN_PKG}/kube_codegen.sh "deepcopy" \
   github.com/stolostron/submariner-addon/generated \
   github.com/stolostron/submariner-addon/pkg/apis \
   "submarinerdiagnoseconfig:v1alpha1" \
