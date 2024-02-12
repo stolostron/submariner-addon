8114a1cf80aa662aaba79926cf47814af59a3c0a
Stephen Kitt <skitt@redhat.com>
2024-02-12 14:07:17 +0100

    Revert "Use kube_codegen.sh instead of generate-groups.sh"
    This reverts commit 6fb8a12b0c98899b67e398145a51df9406ef9cb4.
    
    kube_codegen.sh only defines functions, generate-groups.sh still needs
    to be called to actually perform the generation.
    
    generate-groups.sh relies on generate-internal-groups.sh which needs
    to be made executable. The latter also nukes generated files even when
    verifying; this is disabled here by modifying the vendored files. See
    https://github.com/kubernetes/kubernetes/pull/123261 for the proposed
    upstream fix.
    
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- hack/update-deepcopy.sh
+++ hack/update-deepcopy.sh
@@ -7,14 +7,21 @@ CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-ge
 
 verify="${VERIFY:-}"
 
-GOFLAGS="" bash ${CODEGEN_PKG}/kube_codegen.sh "deepcopy" \
+if [ "$verify" == --verify-only ]; then
+  # Remove the file-nuking portion of the code-generation scripts
+  awk -i inplace '/# Nuke existing files/ { nuke = 1 } /^$/ { nuke = 0 } !nuke' ${CODEGEN_PKG}/generate-internal-groups.sh
+fi
+
+chmod 755 ${CODEGEN_PKG}/generate-internal-groups.sh
+
+GOFLAGS="" bash ${CODEGEN_PKG}/generate-groups.sh "deepcopy" \
   github.com/stolostron/submariner-addon/generated \
   github.com/stolostron/submariner-addon/pkg/apis \
   "submarinerconfig:v1alpha1" \
   --go-header-file ${SCRIPT_ROOT}/hack/empty.txt \
   ${verify}
 
-GOFLAGS="" bash ${CODEGEN_PKG}/kube_codegen.sh "deepcopy" \
+GOFLAGS="" bash ${CODEGEN_PKG}/generate-groups.sh "deepcopy" \
   github.com/stolostron/submariner-addon/generated \
   github.com/stolostron/submariner-addon/pkg/apis \
   "submarinerdiagnoseconfig:v1alpha1" \
