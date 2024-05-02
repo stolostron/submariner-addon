70f28d0b5fdf37611332df8982b9b2c7e26e6382
Maxim Babushkin <mbabushk@redhat.com>
2024-05-02 16:52:52 +0300

    Switch submariner channel to stable-0.18
    Signed-off-by: Maxim Babushkin <mbabushk@redhat.com>

--- pkg/hub/submarinerbrokerinfo/brokerinfo.go
+++ pkg/hub/submarinerbrokerinfo/brokerinfo.go
@@ -32,7 +32,7 @@ const (
 	catalogName                   = "submariner"
 	defaultCatalogSource          = "redhat-operators"
 	defaultCatalogSourceNamespace = "openshift-marketplace"
-	defaultCatalogChannel         = "stable-0.17"
+	defaultCatalogChannel         = "stable-0.18"
 	defaultCableDriver            = "libreswan"
 	defaultInstallationNamespace  = "open-cluster-management-agent-addon"
 	brokerAPIServer               = "BROKER_API_SERVER"
