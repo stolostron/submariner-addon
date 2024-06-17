stolostron/submariner-addon

commit f3f00bf71facb675a4f5603c8e9a4be50d70f1c9

Use K8s kube_codegen.sh instaed of deprecated generate-groups.sh
Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

hack/update-codegen.sh
@@ -11,15 +11,8 @@ verify="${VERIFY:-}"
 
 set -x
 
-if [ "$verify" == --verify-only ]; then
-  # Remove the file-nuking portion of the code-generation scripts
-  awk -i inplace '/# Nuke existing files/ { nuke = 1 } /^$/ { nuke = 0 } !nuke' ${CODEGEN_PKG}/generate-internal-groups.sh
-fi
-
-chmod 755 ${CODEGEN_PKG}/generate-internal-groups.sh
-
 for group in submarinerconfig submarinerdiagnoseconfig; do
-  bash ${CODEGEN_PKG}/generate-groups.sh "client,lister,informer" \
+  bash ${CODEGEN_PKG}/kube_codegen.sh "client,lister,informer" \
     github.com/stolostron/submariner-addon/pkg/client/${group} \
     github.com/stolostron/submariner-addon/pkg/apis \
     "${group}:v1alpha1" \

hack/update-deepcopy.sh
@@ -7,21 +7,14 @@ CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-ge
 
 verify="${VERIFY:-}"
 
-if [ "$verify" == --verify-only ]; then
-  # Remove the file-nuking portion of the code-generation scripts
-  awk -i inplace '/# Nuke existing files/ { nuke = 1 } /^$/ { nuke = 0 } !nuke' ${CODEGEN_PKG}/generate-internal-groups.sh
-fi
-
-chmod 755 ${CODEGEN_PKG}/generate-internal-groups.sh
-
-GOFLAGS="" bash ${CODEGEN_PKG}/generate-groups.sh "deepcopy" \
+GOFLAGS="" bash ${CODEGEN_PKG}/kube_codegen.sh "deepcopy" \
   github.com/stolostron/submariner-addon/generated \
   github.com/stolostron/submariner-addon/pkg/apis \
   "submarinerconfig:v1alpha1" \
   --go-header-file ${SCRIPT_ROOT}/hack/empty.txt \
   ${verify}
 
-GOFLAGS="" bash ${CODEGEN_PKG}/generate-groups.sh "deepcopy" \
+GOFLAGS="" bash ${CODEGEN_PKG}/kube_codegen.sh "deepcopy" \
   github.com/stolostron/submariner-addon/generated \
   github.com/stolostron/submariner-addon/pkg/apis \
   "submarinerdiagnoseconfig:v1alpha1" \

commit 359a1f2daa3b7ce7822c5d0505616dc24ec980c2

Fix codegen using new k8s functions
Commit f3f00bf ("Use K8s kube_codegen.sh instaed of deprecated
generate-groups.sh") broke client code generation; a similar commit
had previously been reverted by commit bc6bdb1 ("Revert "Use
kube_codegen.sh instead of generate-groups.sh""), but that fix is no
longer viable because generate-groups.sh is entirely disabled in 1.30.

Running kube_codegen.sh has no effect now, it only defines functions
without running them. This adjusts hack/update-codegen.sh to use those
functions to generate clients. It also updates the verification
codepath to actually verify code generation; if code generation fails
to do anything in the future, this will now be detected.

Signed-off-by: Stephen Kitt <skitt@redhat.com>

hack/update-codegen.sh
@@ -7,16 +7,28 @@ set -o pipefail
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../../../k8s.io/code-generator)}
 
-verify="${VERIFY:-}"
+if [ "${VERIFY:-}" = "--verify-only" ]; then
+  outprefix=$(mktemp -d -p .)/
+  trap "rm -rf ${outprefix}" EXIT
+else
+  outprefix=
+fi
 
 set -x
 
+. "${CODEGEN_PKG}/kube_codegen.sh"
+
 for group in submarinerconfig submarinerdiagnoseconfig; do
-  bash ${CODEGEN_PKG}/kube_codegen.sh "client,lister,informer" \
-    github.com/stolostron/submariner-addon/pkg/client/${group} \
-    github.com/stolostron/submariner-addon/pkg/apis \
-    "${group}:v1alpha1" \
-    --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.txt \
-    ${verify}
+   kube::codegen::gen_client \
+     --output-dir "${outprefix}pkg/client/${group}" \
+     --output-pkg "github.com/stolostron/submariner-addon/pkg/client/${group}" \
+     --boilerplate "${SCRIPT_ROOT}/hack/boilerplate.txt" \
+     --with-watch \
+     --one-input-api "${group}" \
+     "pkg/apis"
 done
 
+if [ "${VERIFY:-}" = "--verify-only" ]; then
+  diff -urN "${outprefix}/pkg/client" pkg/client >& /dev/null
+  exit $?
+fi