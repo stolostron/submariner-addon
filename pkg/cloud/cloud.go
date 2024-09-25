4f88a8bd00d9e1ab2af0d75c0336a7c4001a8768
Aswin Suryanarayanan <asuryana@redhat.com>
2024-09-25 17:38:51 -0400

    Add support for AWS custom VPC
    Signed-off-by: Aswin Suryanarayanan <asuryana@redhat.com>

--- pkg/cloud/cloud.go
+++ pkg/cloud/cloud.go
@@ -129,6 +129,8 @@ func (f *providerFactory) Get(config *configv1alpha1.SubmarinerConfig, eventsRec
 		return nil, true, err
 	}
 
+	info.SubmarinerConfigAnnotations = config.Annotations
+
 	instance, err := providerFn(info)
 
 	return instance, true, err
