5d54845a676ccc3ef9ec657ae5c22e5ad6781afb
Vishal Thapar <5137689+vthapar@users.noreply.github.com>
2025-02-04 21:15:03 +0530

    Switch submariner channel to stable-0.20
    Switch default submariner channel to stable-0.20
    
    Signed-off-by: Vishal Thapar <5137689+vthapar@users.noreply.github.com>

--- pkg/hub/submarinerbrokerinfo/brokerinfo_test.go
+++ pkg/hub/submarinerbrokerinfo/brokerinfo_test.go
@@ -167,7 +167,7 @@ var _ = Describe("Function Get", func() {
 		When("no SubmarinerConfig is provided", func() {
 			It("should return the defaults", func() {
 				Expect(brokerInfo.CableDriver).To(Equal("libreswan"))
-				Expect(brokerInfo.CatalogChannel).To(Equal("stable-0.19"))
+				Expect(brokerInfo.CatalogChannel).To(Equal("stable-0.20"))
 				Expect(brokerInfo.CatalogName).To(Equal("submariner"))
 				Expect(brokerInfo.CatalogSource).To(Equal("redhat-operators"))
 				Expect(brokerInfo.CatalogSourceNamespace).To(Equal("openshift-marketplace"))
