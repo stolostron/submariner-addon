8a41552682a384f5da6eec150426a727bc5b207b
Vishal Thapar <5137689+vthapar@users.noreply.github.com>
2025-06-25 16:46:40 +0530

    Switch submariner channel to stable-0.21
    Switch default submariner channel to stable-0.21
    
    Signed-off-by: Vishal Thapar <5137689+vthapar@users.noreply.github.com>

--- pkg/hub/submarinerbrokerinfo/brokerinfo.go
+++ pkg/hub/submarinerbrokerinfo/brokerinfo.go
@@ -32,7 +32,7 @@ const (
 	catalogName                   = "submariner"
 	defaultCatalogSource          = "redhat-operators"
 	defaultCatalogSourceNamespace = "openshift-marketplace"
-	defaultCatalogChannel         = "stable-0.20"
+	defaultCatalogChannel         = "stable-0.21"
 	defaultCableDriver            = "libreswan"
 	defaultInstallationNamespace  = "open-cluster-management-agent-addon"
 	brokerAPIServer               = "BROKER_API_SERVER"
