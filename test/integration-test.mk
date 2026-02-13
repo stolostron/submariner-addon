0260e407e5b31d0dfc2c7743e95132f46cc530bb
Tom Pantelis <tompantelis@gmail.com>
2026-02-13 11:39:07 -0500

    Fix integration tests by migrating to modern envtest downloads
    The deprecated storage.googleapis.com/kubebuilder-tools bucket now
    returns HTTP 403 errors, breaking integration test setup. Migrate
    to the official kubernetes-sigs/controller-tools releases on GitHub.
    
    Changes:
    - Update download URL to github.com/kubernetes-sigs/controller-tools
    - Update archive naming to envtest-v{VERSION}-{OS}-{ARCH}.tar.gz format
    - Bump K8S_VERSION from 1.19.2 to 1.30.0 for modern tooling
    
    Fixes CI failure: "Error 22" from curl when downloading
    kubebuilder-tools.
    
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- test/integration-test.mk
+++ test/integration-test.mk
@@ -2,8 +2,8 @@ TEST_TMP :=/tmp
 
 export KUBEBUILDER_ASSETS ?=$(TEST_TMP)/kubebuilder/bin
 
-K8S_VERSION ?=1.19.2
-KB_TOOLS_ARCHIVE_NAME :=kubebuilder-tools-$(K8S_VERSION)-$(GOHOSTOS)-$(GOHOSTARCH).tar.gz
+K8S_VERSION ?=1.30.0
+KB_TOOLS_ARCHIVE_NAME :=envtest-v$(K8S_VERSION)-$(GOHOSTOS)-$(GOHOSTARCH).tar.gz
 KB_TOOLS_ARCHIVE_PATH := $(TEST_TMP)/$(KB_TOOLS_ARCHIVE_NAME)
 
 # download the kubebuilder-tools to get kube-apiserver binaries from it
@@ -11,7 +11,7 @@ ensure-kubebuilder-tools:
 ifeq "" "$(wildcard $(KUBEBUILDER_ASSETS))"
 	$(info Downloading kube-apiserver into '$(KUBEBUILDER_ASSETS)')
 	mkdir -p '$(KUBEBUILDER_ASSETS)'
-	curl -s -f -L https://storage.googleapis.com/kubebuilder-tools/$(KB_TOOLS_ARCHIVE_NAME) -o '$(KB_TOOLS_ARCHIVE_PATH)'
+	curl -s -f -L https://github.com/kubernetes-sigs/controller-tools/releases/download/envtest-v$(K8S_VERSION)/$(KB_TOOLS_ARCHIVE_NAME) -o '$(KB_TOOLS_ARCHIVE_PATH)'
 	tar -C '$(KUBEBUILDER_ASSETS)' --strip-components=2 -zvxf '$(KB_TOOLS_ARCHIVE_PATH)'
 else
 	$(info Using existing kube-apiserver from "$(KUBEBUILDER_ASSETS)")
