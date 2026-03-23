636c2188fb2df8463146615dd753006b749e9598
Stephen Kitt <skitt@redhat.com>
2026-03-24 00:16:46 +0100

    Find code-generator in the module cache
    ... instead of relying on the vendored copy.
    
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -5,7 +5,7 @@ set -o nounset
 set -o pipefail
 
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
-CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../../../k8s.io/code-generator)}
+CODEGEN_PKG=${CODEGEN_PKG:-$(go mod download -json k8s.io/code-generator | jq -r .Dir)}
 
 if [ "${VERIFY:-}" = "--verify-only" ]; then
   outprefix=$(mktemp -d -p .)/
